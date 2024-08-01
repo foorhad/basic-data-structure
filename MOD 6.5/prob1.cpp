f0c237148f8964ad86c239ee5e8a3066c283f7e8
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2024-08-01 13:25:50 +0600

    Add files via upload

---
+++ MOD 6.5/prob1.cpp
@@ -0,0 +1,67 @@
+#include <iostream>  // Input/output streams (cin, cout, cerr, etc.)
+#include <fstream>   // File streams (for file input/output)
+#include <iomanip>   // Input/output manipulators (for formatting)
+#include <string>    // String manipulation
+#include <vector>    // Dynamic arrays
+#include <list>      // Doubly linked list
+#include <queue>     // Queue data structure
+#include <stack>     // Stack data structure
+#include <map>       // Associative array (key-value pairs)
+#include <set>       // Set data structure
+#include <algorithm> // Standard algorithms (sorting, searching, etc.)
+#include <cmath>     // Mathematical functions
+#include <ctime>     // Date and time functions
+#include <cstdlib>   // Standard library utilities (includes functions like `rand()` and `exit()`)
+#include <cstring>   // C-style string functions
+#include <cassert>   // Assertions
+#include <utility>
+#include <limits.h>
+
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_value(Node *&head, int val)
+{
+    Node *newnode = new Node(val);
+    if (head == NULL)
+    {
+        head = newnode;
+        return;
+    }
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = newnode;
+}
+int main()
+{
+    Node *head = NULL;
+    int count = 0;
+    int val;
+    // cout << "Enter the value: ";
+    while (1)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        count++;
+        insert_value(head, val);
+    }
+    cout << count << endl;
+
+    return 0;
+}
