19cf4b84fdd63dd3747092e8496d7758fea6c1c2
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2024-08-16 20:17:17 +0600

    Update min_stack.cpp

--- week 04 conceptual session 01/min_stack.cpp
+++ week 04 conceptual session 01/min_stack.cpp
@@ -1,31 +1,25 @@
 class MinStack {
 public:
     stack<int> s;
-    stack<int> mn;
+    stack<int> min;
     MinStack() {
-        
     }
     
     void push(int val) {
-        if(s.empty())
-        {
-            s.push(val);
-            mn.push(val);
-            return;
-        }
-        mn.push( min(val,mn.top()) );
-        // if(val < mn.top())
-        // {
-        //     mn.push(val);
-        // }
         s.push(val);
+        if(min.empty()){
+            min.push(val);
+        }
+        else{
+            if(val <= min.top()){
+                min.push(val);
+            }
+        }
     }
     
     void pop() {
-        if(s.empty()) return;
-        else{
+        if(s.top() == min.top()) min.pop();
         s.pop();
-        mn.pop();}
     }
     
     int top() {
@@ -33,7 +27,6 @@ public:
     }
     
     int getMin() {
-        return mn.top();
-        
+        return min.top();
     }
 };
