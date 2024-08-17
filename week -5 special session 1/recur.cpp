a4418942c1215c3cce7c64b303202f23656f0a1b
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2024-08-17 23:16:10 +0600

    Add files via upload

---
+++ week -5 special session 1/recur.cpp
@@ -0,0 +1,31 @@
+#include <iostream>     // Input/output streams (cin, cout, cerr, etc.)
+#include <fstream>      // File streams (for file input/output)
+#include <iomanip>      // Input/output manipulators (for formatting)
+#include <string>       // String manipulation
+#include <vector>       // Dynamic arrays
+#include <list>         // Doubly linked list
+#include <queue>        // Queue data structure
+#include <stack>        // Stack data structure
+#include <map>          // Associative array (key-value pairs)
+#include <set>          // Set data structure
+#include <algorithm>    // Standard algorithms (sorting, searching, etc.)
+#include <cmath>        // Mathematical functions
+#include <ctime>        // Date and time functions
+#include <cstdlib>      // Standard library utilities (includes functions like `rand()` and `exit()`)
+#include <cstring>      // C-style string functions
+#include <cassert>      // Assertions
+#include <utility>
+#include <limits.h>
+ 
+using namespace std;
+void recur(int val){
+    if(val == 6) return;
+    recur(val+1);
+    cout<<val<<endl;
+}
+int main()
+{
+    recur(1);
+    
+    return 0;
+}
