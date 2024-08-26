fce580f4e866314f72e221af653aac009b63ab6b
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2024-08-26 12:27:08 +0600

    Update binary_tree_input.cpp

--- MOD 18/binary_tree_input.cpp
+++ MOD 18/binary_tree_input.cpp
@@ -1,22 +1,4 @@
-#include <iostream>     // Input/output streams (cin, cout, cerr, etc.)
-#include <fstream>      // File streams (for file input/output)
-#include <iomanip>      // Input/output manipulators (for formatting)
-#include <string>       // String manipulation
-#include <vector>       // Dynamic arrays
-#include <list>         // Doubly linked list
-#include <queue>        // Queue data structure
-#include <stack>        // Stack data structure
-#include <map>          // Associative array (key-value pairs)
-#include <set>          // Set data structure
-#include <algorithm>    // Standard algorithms (sorting, searching, etc.)
-#include <cmath>        // Mathematical functions
-#include <ctime>        // Date and time functions
-#include <cstdlib>      // Standard library utilities (includes functions like `rand()` and `exit()`)
-#include <cstring>      // C-style string functions
-#include <cassert>      // Assertions
-#include <utility>
-#include <limits.h>
- 
+#include <bits/stdc++.h>
 using namespace std;
 class Node
 {
