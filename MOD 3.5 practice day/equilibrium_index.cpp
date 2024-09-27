17caccfdb53d773b1a85fee44ae56145b3c9a994
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2024-09-27 11:53:31 +0600

    Update equilibrium_index.cpp

--- MOD 3.5 practice day/equilibrium_index.cpp
+++ MOD 3.5 practice day/equilibrium_index.cpp
@@ -1,10 +1,6 @@
 #include <bits/stdc++.h>
-#define ll long long int
-#define pi pair<ll, ll>
 #define nl '\n'
 #define fast() ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
-const long long int INF = 1e18+5;
-const int N = 1e6 + 5;
 using namespace std;
 
 
