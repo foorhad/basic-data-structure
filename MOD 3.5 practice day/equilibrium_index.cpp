9a021391583626daea00cef0df28bcbd874f9c5d
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2024-09-27 11:22:32 +0600

    Update equilibrium_index.cpp

--- MOD 3.5 practice day/equilibrium_index.cpp
+++ MOD 3.5 practice day/equilibrium_index.cpp
@@ -1,23 +1,43 @@
 #include <bits/stdc++.h>
+#define nl '\n'
+#define fast() ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
 using namespace std;
+
 
 int main()
 {
-    int n;
-    cin >> n;
-    vector<int> a(n);
+ fast();
+    int n;cin>>n;
+    int ar[n];
     for (int i = 0; i < n; i++)
     {
-        cin >> a[i];
+        cin>>ar[i];
     }
-    vector<int> presum(n);
-    presum[0] = a[0];
-    for (int i = 2; i < n; i++)
+    int ans_index;
+    int left_sum=0;
+    int right_sum=0;
+    for (int i = 0; i < n; i++)
     {
-        presum[i] = a[i - 1] + presum[i];
+        ans_index=i+1;
+       for (int j = 0; j < n; j++)
+       {
+        if(j<ans_index){
+            left_sum += ar[j];
+        }
+        else if (j==ans_index)continue;
+        else
+        {
+            right_sum+=ar[j];
+        }
+       }
+       if(left_sum==right_sum)break;
+       else{
+        left_sum=0, right_sum=0;
+       }
     }
-    for (int e : a)
-        cout << e <<endl;
+    cout<<ans_index<<nl;
+    
+    
 
     return 0;
 }
